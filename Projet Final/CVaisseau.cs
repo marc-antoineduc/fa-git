ecb46b5ec91eae133cfbffc51a07dfcf79c3e99e
INFO-324-1A-123\utilisateur <marcantoine_duc@hotmail.com>
2016-11-21 10:10:49 -0500

    Création de Vaisseaux

---
+++ Projet Final/CVaisseau.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final
+{
+    class CVaisseau
+    {
+        protected string type = "";
+        protected int capaciteMax = 0, papier = 0, verre = 0, plastique = 0, ferraille = 0, terre = 0;
+        public CVaisseau(string type, int capaciteMax)
+        {
+            this.type = type; this.capaciteMax = capaciteMax;
+        }
+        public string Type
+        {
+            get { return type; }
+        }
+        public int CapaciteMax
+        {
+            get { return CapaciteMax; }
+        }
+    }
+}
---
+++ Projet Final/CVaisseauCargo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final
+{
+    class CVaisseauCargo:CVaisseau
+    {
+        public CVaisseauCargo() : base("Vaisseau Cargo",367)
+        {
+
+        }
+    }
+}
